e5ade6e9dc04080ec31fd908b0ca248ead7a0598
SSviatoslaWW <sviatoslaw7779@gmail.com>
2025-10-15 14:19:39 +0300

    Initial Commit

---
+++ Kursova/KursovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KursovaApp.swift
+//  Kursova
+//
+//  Created by Svituk on 15.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct KursovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KursovaTests/KursovaTests.swift
@@ -0,0 +1,17 @@
+//
+//  KursovaTests.swift
+//  KursovaTests
+//
+//  Created by Svituk on 15.10.2025.
+//
+
+import Testing
+@testable import Kursova
+
+struct KursovaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
